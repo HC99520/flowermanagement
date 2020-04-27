6dc838df33875b517a55e61ba576d5571eb51a2f
HC99520 <1902708839@qq.com>
2020-04-27 22:12:15 +0800

    员工查询所有

--- src/main/java/com/lyg/flowermanagement/Controller/CustomerController.java
+++ src/main/java/com/lyg/flowermanagement/Controller/CustomerController.java
@@ -12,8 +12,6 @@ import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
-
 @RestController
 @RequestMapping("/EmployeeController")
 @CrossOrigin("*")
